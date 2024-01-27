af4c40aa0390cdb964a2f1a3acb89c90d300226b
CodeHappy4U <happypatel9825@gmail.com>
2024-01-27 14:42:19 +0530

    1. Component Reuse

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,13 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>First Page</h1>
+      <User/>
+    
+    {/* REUSE COMPONENTS */}
+      <User/>
+      <User/>
+      <User/>
+      
     </main>
   );
 }
