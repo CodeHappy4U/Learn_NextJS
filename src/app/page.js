faa26087274dad6d8915475d2aac8cfa309a5474
CodeHappy4U <happypatel9825@gmail.com>
2024-01-29 14:08:31 +0530

    2.6 COMPNENT INSIDE COMPONENT

--- src/app/page.js
+++ src/app/page.js
@@ -6,17 +6,23 @@ import styles from "./page.module.css";
 export default function Home() {
 
   // USING REACT STATE
-  // const [name,setName] = useState("Happy") //TEMPORARY NAME
-  let data="Alpit"
+  const [name,setName] = useState("Happy") //TEMPORARY NAME
+  
   const mango=()=>{
-    // setName("Raja") //USING SETTING STATE
-      data ="Arjun"
+    setName("Raja") //USING SETTING STATE
+  }
+
+  //COMPNENT INSIDE COMPONENT
+  const InnerComp=()=>{
+    return(
+      <h1>Inner Component</h1>
+    )
   }
   return (
     <main className={styles.main}> 
-    {/* HTML INSIDE JSX */}
-      <h1>Events, Function and State {data}</h1>
+      <h1>Events, Function and State {name}</h1>
       <button onClick={()=>mango()}>Click Me</button> 
+      <InnerComp/>
     </main>
-  );
+  )
 }
