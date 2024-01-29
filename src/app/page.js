073ed753210c57420999588e9edf2be643109d4a
CodeHappy4U <happypatel9825@gmail.com>
2024-01-29 12:02:16 +0530

    2.2 CLIENT SIDE RENDERING

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
-
+'use client'
 import styles from "./page.module.css";
+'use client' // WRONG WAY TO DO THIS 
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Events, Function and State</h1>
-      <button onClick={()=>alert ("Hello")}>Click Me</button> //USE AS A SERVER COMPONENT
+
+      // USE CLIENT SIDE RENDERING
+      <button onClick={()=>alert ("Hello")}>Click Me</button> 
     </main>
   );
 }
