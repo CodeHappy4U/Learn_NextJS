e4dd83255e05257d26f18aed59058ba0fe8ae07a
CodeHappy4U <happypatel9825@gmail.com>
2024-01-29 13:33:15 +0530

    2.3 USING ONCLICK AND ARROW FUNCTION

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,17 @@
 'use client'
 import styles from "./page.module.css";
-'use client' // WRONG WAY TO DO THIS 
+
 
 export default function Home() {
+  const mango= ()=>{
+    alert("Fruit")
+  }
   return (
     <main className={styles.main}>
       <h1>Events, Function and State</h1>
 
-      // USE CLIENT SIDE RENDERING
-      <button onClick={()=>alert ("Hello")}>Click Me</button> 
+      {/* USING ONCLICK AND ARROW FUNCTION */}
+      <button onClick={mango}>Click Me</button> 
     </main>
   );
 }
