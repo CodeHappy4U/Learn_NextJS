e0f7fb4cd2e1f26072c49f8b46fc71e8cec8f26c
CodeHappy4U <happypatel9825@gmail.com>
2024-01-30 12:23:17 +0530

    5.2 MAKE A COMMON FUNCTION & PASSING THROUGH CALLING METHODS

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -5,16 +5,19 @@ import Link from "next/link";
 
 const Login=()=>{
     const router = useRouter();
+    const navigate = (page)=>{
+        router.push("/login/" + page)
+    }
     return(
         <div>
             <h1>Login Page</h1>
             <Link  href= "/" >Go To Home Page</Link> 
             <br/>
             <br/>
-            <button  onClick={() => router.push('/login/loginstudent')}> Go to Login Student Page </button>
+            <button  onClick={() => navigate('loginstudent')}> Go to Login Student Page </button>
             &nbsp;
             &nbsp;
-            <button  onClick={() => router.push('/login/loginteacher')}> Go to Login Teacher Page </button>
+            <button  onClick={() => navigate('loginteacher')}> Go to Login Teacher Page </button>
             
         </div>
     )
